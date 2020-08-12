c659b1f1d308c81104204488e44722d4d7fe776b
roshan395 <32610544+roshan395@users.noreply.github.com>
2020-08-13 01:46:42 +0530

    last revert
    reverted to the old version as it was giving error

--- api/routes/products.js
+++ api/routes/products.js
@@ -2,46 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Handling GET requests to /products'
-    });
+  res.status(200).json({
+    message: 'server is up and running. build more routes'
+  });
 });
 
-router.post('/', (req, res, next) => {
-    const product = {
-        name: req.body.name,
-        price: req.body.price
-    };
-    res.status(201).json({
-        message: 'Handling POST requests to /products',
-        createdProduct: product
-    });
-});
-
-router.get('/:productId', (req, res, next) => {
-    const id = req.params.productId;
-    if (id === 'special') {
-        res.status(200).json({
-            message: 'You discovered the special ID',
-            id: id
-        });
-    } else {
-        res.status(200).json({
-            message: 'You passed an ID'
-        });
-    }
-});
-
-router.patch('/:productId', (req, res, next) => {
-    res.status(200).json({
-        message: 'Updated product!'
-    });
-});
-
-router.delete('/:productId', (req, res, next) => {
-    res.status(200).json({
-        message: 'Deleted product!'
-    });
-});
 
 module.exports = router;
